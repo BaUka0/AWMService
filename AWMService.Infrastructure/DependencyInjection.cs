8a11cc6d1143b8e5b243eed8ee8133ef1e46bc70
BaUka0 <bauirzhan.bisanov@gmail.com>
2025-08-01 13:33:51 +0500

    Добавил DependencyInjection для слоя Infrastructure Подключил БД

---
+++ AWMService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿
+
+using AWMService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
+
